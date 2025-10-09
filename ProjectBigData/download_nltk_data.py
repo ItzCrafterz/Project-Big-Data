0aaa71c262265b795d88d927d0de00a27c699fcb
Gilang Gallan Indrana <45196130+ItzCrafterz@users.noreply.github.com>
2025-10-09 17:00:29 +0700

    Delete ProjectBigData directory

--- ProjectBigData/download_nltk_data.py
+++
@@ -1,8 +0,0 @@
-# download_nltk_data.py
-import nltk
-
-print("Mengunduh 'punkt'...")
-nltk.download('punkt')
-print("Mengunduh 'stopwords'...")
-nltk.download('stopwords')
-print("Pengunduhan data NLTK selesai.")
